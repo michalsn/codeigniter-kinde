2854136527de454921bcf53cb81515078bab4d03
michalsn <michal@sniatala.pl>
2023-07-26 09:29:41 +0200

    phpstan updates

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -2,18 +2,14 @@
 
 namespace Michalsn\CodeIgniterKinde\Config;
 
-$routes->get('login', static function () {
-    return service('kinde')->login();
-}, ['as' => 'login']);
+use Config\Services;
 
-$routes->get('register', static function () {
-    return service('kinde')->register();
-}, ['as' => 'register']);
+$routes = Services::routes();
 
-$routes->get('logout', static function () {
-    return service('kinde')->logout();
-}, ['as' => 'logout', 'filter' => 'kinde']);
+$routes->get('login', static fn () => service('kinde')->login(), ['as' => 'login']);
 
-$routes->get('callback', static function () {
-    return service('kinde')->callback();
-}, ['as' => 'callback']);
+$routes->get('register', static fn () => service('kinde')->register(), ['as' => 'register']);
+
+$routes->get('logout', static fn () => service('kinde')->logout(), ['as' => 'logout', 'filter' => 'kinde']);
+
+$routes->get('callback', static fn () => service('kinde')->callback(), ['as' => 'callback']);
