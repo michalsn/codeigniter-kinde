15a7c633f6c72e539d9d26cdf88d7e1c8356dff2
michalsn <michal@sniatala.pl>
2023-07-27 16:27:10 +0200

    cs fix

--- src/Config/Kinde.php
+++ src/Config/Kinde.php
@@ -14,7 +14,11 @@ class Kinde extends BaseConfig
     public string $clientSecret      = '';
     public string $grantType         = GrantType::authorizationCode;
 
+    /**
+     * User profile defaults
+     */
     public string $defaultLanguage = 'pl';
+
     public string $defaultTimezone = 'Europe/Warsaw';
 
     public function afterCallbackSuccess()
